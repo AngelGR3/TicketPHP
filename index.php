d61629d89b47aaae47af8c933c42ef5928558382
AngelGR3 <angelguardador3@gmail.com>
2020-06-17 17:28:55 -0700

    Diseño de ticket

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ $connector = new WindowsPrintConnector($nombre_impresora);
 $printer = new Printer($connector);
 $printer->setJustification(Printer::JUSTIFY_CENTER);
 
-$logo = EscposImage::load("Perfil.png", false);
+$logo = EscposImage::load("Karina.jpg", false);
 $printer->bitImage($logo);
 
 /*
@@ -36,17 +36,35 @@ Imprimimos un mensaje. Podemos usar
 el salto de línea o llamar muchas
 veces a $printer->text()
  */
-$printer->setTextSize(2, 2);
-$printer->text("BlazarSoft");
-
-$printer->setTextSize(2, 1);
+$printer->setTextSize(1, 2);
+$printer->text("Calle 10 Avenida 28 Tel: 633-121-0748\n Agua Prieta Son.\n");
+$printer->text("______________________________________\n");
 $printer->feed();
-$printer->text("Empresa\n\nSoftware");
+$printer->setTextSize(1, 1);
+$printer->feed();
+$printer->text("No. Cliente: 24          Fecha: 23/01/2020\n");
+$printer->text("Credito: 2\n");
+$printer->text("Cliente: Diana Luara Lopez B.\n");
+$printer->text("______________________________________\n");
+$printer->text("No. pago: 4     Fecha: 23/01/2020\n");
+$printer->text("Abono________________________$200\n");
+$printer->text("Intereses_____________________$0.00\n");
+$printer->text("Descuento_____________________$0.00\n");
+$printer->text("______________________________________\n");
+$printer->text("Total a pagar_________________$200.00\n");
+$printer->feed();
+$printer->text("Saldo actual___________$1400.00\n");
+$printer->text("Proximo pago :23/02/2020\n");
+$printer->feed();
+$printer->setTextSize(1, 2);
+$printer->text("Gracias por su preferencia\n");
+$printer->text("---CUIDE SU CREDITO---");
+
 /*
 Hacemos que el papel salga. Es como
 dejar muchos saltos de línea sin escribir nada
  */
-$printer->feed(15);
+$printer->feed(3);
 
 /*
 Cortamos el papel. Si nuestra impresora
